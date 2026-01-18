c313d02b304900ca3e7f4f9d6e27c682fc8a63ef
mwijanarko1 <mikhail.wijanarko2003@gmail.com>
2026-01-18 03:52:23 +0000

    chore: update babel config formatting and add worklets plugin comment
    - Remove AI-Proxy-Backend submodule
    - Update babel.config.js formatting (quotes)
    - Add commented react-native-worklets plugin reference

--- PromptPal/babel.config.js
+++ PromptPal/babel.config.js
@@ -1,10 +1,15 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel'
     ],
+    plugins: [
+      // If you need custom plugin for worklets, use:
+      // 'react-native-worklets/plugin'
+      // DO NOT also include react-native-reanimated/plugin
+    ]
   };
 };
 
